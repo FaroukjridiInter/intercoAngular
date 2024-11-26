62f9fb6e7a968704500e80ef8facfed446b5bed2
FaroukjridiInter <jridi.farouk@gmail.com>
2024-11-26 18:54:44 +0000

    Angular - 5. Les outputs et input models

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,28 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PlayingCardComponent } from './components/playing-card/playing-card.component';
 import { Monster } from './monster/monster.model';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [PlayingCardComponent],
+  imports: [ SearchBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+	
+	count: number = 0;
 	monster1!: Monster;
+	search: string = '';
 
 	constructor() {
 		this.monster1 = new Monster();
 		this.monster1.name = "Pik";
 		this.monster1.hp = 40;
 		this.monster1.figureCaption = "N°002 Pik";
+	}
+	increaseCount() {
+		this.count++;
 	}
 }
--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, model, output, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search-bar',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.css'
 })
 export class SearchBarComponent {
+
+	searchButtonClicked = output<void>();
+
+	search = model('blala');
+
+	searchClick() {
+		this.searchButtonClicked.emit();
+	}
 
 }
