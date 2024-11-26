622aa10cf273fe85d6c45f16b1377227fc012a0f
Farouk95 <farouk.jridi@esprit.tn>
2024-11-26 15:45:10 +0100

    Angular - 4. Les inputs et signal inputs

--- src/app/components/playing-card/playing-card.component.ts
+++ src/app/components/playing-card/playing-card.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, InputSignal, input} from '@angular/core';
+import { Monster } from '../../monster/monster.model';
 
 @Component({
-  selector: 'app-playing-card',
-  imports: [],
-  templateUrl: './playing-card.component.html',
-  styleUrl: './playing-card.component.css'
+	selector: 'app-playing-card',
+	standalone: true,
+	imports: [CommonModule],
+	templateUrl: './playing-card.component.html',
+	styleUrl: './playing-card.component.css'
 })
 export class PlayingCardComponent {
+
+	monster: InputSignal<Monster> = input(new Monster());
 
 }
