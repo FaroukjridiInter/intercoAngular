cf5bf30410e413f78a45c4d6c5f61360f68a62d2
FaroukjridiInter <jridi.farouk@gmail.com>
2024-11-27 16:55:48 +0000

    Angular - 8. Les services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component, computed, effect, model, signal } from '@angular/core';
+import { Component, computed, effect, inject, model, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PlayingCardComponent } from './components/playing-card/playing-card.component';
 import { Monster } from './monster/monster.model';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { MonsterType } from './utils/monster.utils';
 import { CommonModule } from '@angular/common';
+import { MonsterService } from './services/monster/monster.service';
 
 @Component({
   selector: 'app-root',
@@ -14,57 +15,32 @@ import { CommonModule } from '@angular/common';
 })
 export class AppComponent {
 
+	monsterService = inject(MonsterService);
 	
 	count: number = 0;
-	monsters!: Monster[];
+	monsters= signal<Monster[]>([]);
 	search = model('');
  	filteredMonsters = computed(() => {
- 		return this.monsters.filter(monster => monster.name.includes(this.search()));
+ 		return this.monsters().filter(monster => monster.name.includes(this.search()));
  	});
-	selectedMonsterIndex = signal( 0);
+	/*selectedMonsterIndex = signal( 0);
 	selectedMonster = computed(()=>{
-		return this.monsters[this.selectedMonsterIndex()];
-	})
+		return this.monsters([this.selectedMonsterIndex()];
+	})*/
 
 	constructor() {
-		this.monsters = [];
-		effect(()=>{
+		this.monsters.set(this.monsterService.getAll());
+	/*	effect(()=>{
 			console.log(this.selectedMonster());
-		})
-
-		this.monsters = [];
-		const monster1 = new Monster();
-		monster1.name = "Pik";
-		monster1.hp = 40;
-		monster1.figureCaption = "N°002 Pik";
-		this.monsters.push(monster1);
-
-		const monster2 = new Monster();
-		monster2.name = "Car";
-		monster2.image = "assets/img/car.png";
-		monster2.type = MonsterType.WATER;
-		monster2.hp = 60;
-		monster2.figureCaption = "N°003 Car";
-		this.monsters.push(monster2);
-
-		const monster3 = new Monster();
-		monster3.name = "Bulb";
-		monster3.image = "assets/img/bulb.png";
-		monster3.type = MonsterType.PLANT;
-		monster3.hp = 60;
-		monster3.figureCaption = "N°004 Bulb";
-		this.monsters.push(monster3);
-
-		const monster4 = new Monster();
-		monster4.name = "Sala";
-		monster4.image = "assets/img/sala.png";
-		monster4.type = MonsterType.FIRE;
-		monster4.hp = 60;
-		monster4.figureCaption = "N°004 Sala";
-		this.monsters.push(monster4);
+		})*/
 	}
 
-	toggleMonster() {
+	addGenericMonster() {
+		const monster = new Monster();
+		this.monsterService.add(monster);
+		this.monsters.set(this.monsterService.getAll());
+	}
+	/*toggleMonster() {
 		this.selectedMonsterIndex.set((this.selectedMonsterIndex() + 1) % this.monsters.length);
-	}
+	}*/
 }
